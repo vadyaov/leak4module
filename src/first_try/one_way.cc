b57f8f57e085f47b8f3c8d10a70e4819f6d72f4a
Вадим Мякиш <vamyakish@mail.ru>
2024-01-10 17:35:35 +0300

    deleted waste

--- src/first_try/one_way.cc
+++
@@ -1,8 +0,0 @@
-#include "one_way.h"
-#include "filereader.h"
-
-void OneWayRelease::LoadData(const std::string& filename) {
-  FileReader reader(filename);
-  times_ = reader.GetTimeArray();
-  releases_ = reader.GetData();
-}
